8b48d9a8fcb901552b987f644c2ed665fdcf93e3
Zappar <hello@zappar.com>
2021-03-01 09:42:12 +0000

    Example for Zappar Unity

---
+++ Assets/Scripts/playAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playAnimation : MonoBehaviour
+{
+
+  public AnimationClip jump;
+  Animation anim;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    anim = GetComponent<Animation>();
+    anim.clip = jump;
+  }
+  public void playJumpAnimation()
+  {
+    anim.Play();
+  }
+}
